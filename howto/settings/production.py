a52133a18d48e407114cefa50c3cb0cc56728bc4
Baris Yuksel <brsyuksel@gmail.com>
2014-02-11 15:38:10 +0200

    initial commit

---
+++ howto/settings/production.py
@@ -0,0 +1,16 @@
+
+ADMINS = MANAGERS = ()
+
+ALLOWED_HOSTS = []
+
+SECRET_KEY = ''
+
+DATABASES = {
+	'default': {
+		'ENGINE': '',
+		'HOST': '',
+		'NAME': '',
+		'USER': '',
+		'PASSWORD': ''
+	}
+}
